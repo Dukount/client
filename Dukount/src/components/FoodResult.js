e837926669f7670c5cbb909f6cc5c9f53a2a98a5
dennymiloen <dennymiloe@gmail.comn>
2017-11-02 22:09:01 +0700

    initial-UI

--- Dukount/src/components/FoodResult.js
+++ Dukount/src/components/FoodResult.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { 
+import {
   View,
   Text,
   Picker,
   Button,
-  TouchableOpacity 
+  TouchableOpacity,
+  Dimensions
 } from "react-native"
 import { connect } from "react-redux"
 
@@ -21,17 +22,18 @@ class FoodResult extends Component {
       dinner: 'cost',
       breakfastResult: '',
       lunchResult: '',
-      dinnerResult: ''
+      dinnerResult: '',
+      activityDay: 20
     }
   }
 
   getResult () {
-    let location = 'Tebet'
+    let location = 'Gandaria'
     let breakfastSort = this.state.breakfast
     let lunchSort = this.state.lunch
     let dinnerSort = this.state.dinner
     let snackSort = this.state.snack
-    let queryBreakfast = 'q=' + location + '&sort=' + breakfastSort + '&order=asc'
+    let queryBreakfast = 'q=' + location + '&cuisines=114&sort=' + breakfastSort + '&order=asc'
     let queryLunch = 'q=' + location + '&cuisines=114%2C235&sort=' + lunchSort + '&order=asc'
     let queryDinner = 'q=' + location + '&cuisines=168%2C40%2C181&sort=' + dinnerSort + '&order=asc'
     let querySnack = 'q=' + location + '&cuisines=270&sort=' + snackSort + '&order=asc'
@@ -41,53 +43,79 @@ class FoodResult extends Component {
     this.props.sortDataDinner(queryDinner)
   }
 
+  resultOutcome () {
+    const sumFoodOutcomeResult = this.props.breakfastPrice[0] + this.props.lunchPrice[0] + this.props.dinnerPrice[0]
+    const resultFoodFinal = parseFloat(sumFoodOutcomeResult / 1000000 * this.state.activityDay).toFixed(3)
+    if(resultFoodFinal && isNaN(resultFoodFinal) === false) {
+       return (<Text>Result Rp {resultFoodFinal}.000</Text>)
+    }else {
+      return (<Text>Result -</Text>)
+    }
+  }
+
+
   render () {
     console.log('sarapan ',this.props.breakfastPrice)
     console.log('siang ',this.props.lunchPrice)
     console.log('malem ',this.props.dinnerPrice)
     return (
-      <View style={{flexDirection: 'column', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'left', width: 100, height: 50}}>
-          <Text>Breakfast:</Text>
+      <View style={{flexDirection: 'column', flex: 2}}>
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor:'#57A8F8', paddingHorizontal: 10, justifyContent: 'flex-end', marginBottom: 1}}>
+        <View style={{width: 240}}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18, textAlign: 'left'}}>Breakfast:</Text>
+        </View>
+        <View>
           <Picker
-          style={{width: 100}} 
+          style={{width: 100}}
           selectedValue={this.state.breakfast}
           onValueChange={(itemValue, itemIndex) => this.setState({breakfast:
           itemValue})}>
-          <Picker.Item label='Cost' value='cost' color='#57A8F8'/>
-          <Picker.Item label='Rating' value='rating' color='#57A8F8'/>
+          <Picker.Item label='Cost' value='cost'/>
+          <Picker.Item label='Rating' value='rating'/>
           </Picker>
-        </View >
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-          <Text>Lunch:</Text>
+        </View>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor:'#57A8F8', paddingHorizontal: 10, justifyContent: 'flex-end', marginBottom: 1}}>
+        <View style={{width: 240}}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18}}>Lunch:</Text>
+        </View>
+        <View>
           <Picker
-          style={{width: 100}}  
+          style={{width: 100}}
           selectedValue={this.state.lunch}
           onValueChange={(itemValue, itemIndex) => this.setState({lunch:
           itemValue})}>
-          <Picker.Item label='Cost' value='cost' color='#57A8F8'/>
-          <Picker.Item label='Rating' value='rating' color='#57A8F8'/>
+          <Picker.Item label='Cost' value='cost'/>
+          <Picker.Item label='Rating' value='rating'/>
           </Picker>
         </View>
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', marginBottom: 50}}>
-          <Text>Dinner:</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor:'#57A8F8', paddingHorizontal: 10, justifyContent: 'flex-end'}}>
+        <View style={{width: 240}}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18}}>Dinner:</Text>
+        </View>
           <Picker
-          style={{width: 100}}  
+          style={{width: 100}}
           selectedValue={this.state.dinner}
           onValueChange={(itemValue, itemIndex) => this.setState({dinner:
           itemValue})}>
-          <Picker.Item label='Cost' value='cost' color='#57A8F8'/>
-          <Picker.Item label='Rating' value='rating' color='#57A8F8'/>
+          <Picker.Item label='Cost' value='cost'/>
+          <Picker.Item label='Rating' value='rating'/>
           </Picker>
         </View>
         <View style={{marginBottom: 120}}>
         <TouchableOpacity onPress={() => this.getResult()}>
-        <View style={{backgroundColor:'#57A8F8', width:150, height:30, alignSelf:'center', padding: 5, alignItems: 'center', borderRadius: 5}}>
-          <Text style={{color:'white', fontWeight: 'bold'}}>Submit</Text>
+        <View style={{backgroundColor:'#1d81e5', padding: 15, alignItems: 'center', borderRadius: 3, width: 300, alignSelf: 'center'}}>
+          <Text style={{color:'white', fontWeight: 'bold'}}>Check Price</Text>
         </View>
         </TouchableOpacity>
         </View>
         <View style={{marginBottom: 200}}>
+        <View>
+        {
+          this.resultOutcome()
+        }
+        </View>
         <Text>Breakfast: Rp {parseFloat(this.props.breakfastPrice / 1000).toFixed(3)}</Text>
         <Text>Lunch: Rp {parseFloat(this.props.lunchPrice / 1000).toFixed(3)}</Text>
         <Text>Dinner: Rp {parseFloat(this.props.dinnerPrice / 1000).toFixed(3)}</Text>
@@ -96,6 +124,7 @@ class FoodResult extends Component {
     )
   }
 }
+
 const mapDispatchToProps = (dispatch) => {
   return {
     sortDataBreakfast: (data) => dispatch(getBreakfast(data)),
