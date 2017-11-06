e645619b5a3284b35abcb03912248b661cab4598
dennymiloen <dennymiloe@gmail.comn>
2017-11-06 15:47:07 +0700

    get-all-final-result-value

--- Dukount/src/components/FinalResult.js
+++ Dukount/src/components/FinalResult.js
@@ -13,6 +13,54 @@ class FinalResult extends Component {
   }
   constructor() {
     super()
+    this.state = {
+      transportResult: null,
+      resultFinal: null,
+      userOutcome: '-',
+      salaryRestUser: '-'
+    }
+  }
+
+  transportationResult () {
+    var uber = this.props.uberTransportPrice
+    var trafi = this.props.trafiTransportPrice
+    var userChoice = this.props.userTransportMode
+    var transportResult = null
+
+    if (userChoice === true) {
+      this.setState({
+        transportResult : trafi
+      })
+    } else {
+      this.setState({
+        transportResult : uber
+      })
+    }
+  }
+
+  finalAllResult () {
+    let salary = this.props.userSalary
+    let food = this.props.foodOutcome
+    let transport = this.state.transportResult
+    let sumOutcome = food + (+transport)
+    let restSalary = (+salary) - sumOutcome
+    console.log('food ', food);
+    console.log('transport ', transport);
+    console.log('hasil ', sumOutcome);
+    console.log('gaji ', salary);
+
+    this.setState({
+      userOutcome: parseFloat(sumOutcome),
+      salaryRestUser: parseFloat(restSalary)
+    })
+  }
+
+  componentWillMount() {
+    this.transportationResult()
+  }
+
+  componentDidMount() {
+    this.finalAllResult()
   }
 
   render () {
@@ -22,7 +70,9 @@ class FinalResult extends Component {
       <View>
         <Text>Your salary : {this.props.userSalary}</Text>
         <Text>Your Food Outcome: IDR {resultFood}.000</Text>
-        <Text>Your Transportation Outcome: </Text>
+        <Text>Your Transportation Outcome: {this.state.transportResult}</Text>
+        <Text>Your Final Outcome : {this.state.userOutcome}</Text>
+        <Text>The rest of Salary  : {this.state.salaryRestUser}</Text>
       </View>
     )
   }
@@ -31,7 +81,11 @@ class FinalResult extends Component {
 const mapStateToProps = (state) => {
   return {
     userSalary: state.salaryReducer.salary,
-    foodOutcome: state.price.foodFinal
+    foodOutcome: state.price.foodFinal,
+    userTransportMode: state.MapReducer.transportMode,
+    uberTransportPrice: state.MapReducer.uberFare,
+    trafiTransportPrice: state.MapReducer.trafiFare
+
   }
 }
 
