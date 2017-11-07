7e09828771a05eb8adf249ee7402e9083ae77992
Yosua1011 <yosua1011@gmail.com>
2017-11-08 05:08:07 +0700

    Splashscreen test done on MapExample

--- MapExample/src/components/Home.js
+++ MapExample/src/components/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, TouchableHighlight, Text } from 'react-native';
 import { connect } from 'react-redux'
+import SplashScreen from 'react-native-splash-screen'
 
 class Home extends Component {
+  componentDidMount() {
+      // do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+    }
 
   render() {
     const { navigate } = this.props.navigation
