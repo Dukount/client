93481b571eb55a67ee14cfcb9d06643c8761e3e2
Yosua1011 <yosua1011@gmail.com>
2017-11-07 16:05:41 +0700

    Splashscreen showed up but app closed suddenly

--- MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
+++ MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
@@ -3,6 +3,7 @@ package com.mapexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNGooglePlacesPackage(),
             new MapsPackage()
       );
