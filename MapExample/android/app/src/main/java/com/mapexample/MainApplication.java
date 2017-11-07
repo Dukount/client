137256c404752dfad9782a124b2d5a48c1c5a8eb
Yosua1011 <yosua1011@gmail.com>
2017-11-07 16:21:36 +0700

    MapExample replaced with the old one

--- MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
+++ MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
@@ -3,7 +3,6 @@ package com.mapexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new RNGooglePlacesPackage(),
             new MapsPackage()
       );
