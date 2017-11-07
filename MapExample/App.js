393676fa8abe6a7ce72ee1b04f23a990bbd56e87
Yosua1011 <yosua1011@gmail.com>
2017-11-08 04:00:00 +0700

    MapExample Splashscreen always crash

--- MapExample/App.js
+++ MapExample/App.js
@@ -8,8 +8,16 @@ import React, { Component } from 'react';
 import Navigator from './src/config/Navigator'
 import { Provider } from 'react-redux';
 import store from './src/store';
+import SplashScreen from 'react-native-splash-screen'
+
 
 export default class App extends Component<{}> {
+  componentDidMount() {
+    	// do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+    }
+
   render() {
     // console.log(this.state.region)
     return (
--- MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
+++ MapExample/android/app/src/main/java/com/mapexample/MainApplication.java
@@ -3,7 +3,7 @@ package com.mapexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTSplashScreenPackage(),
+            new SplashScreenReactPackage(),
             new RNGooglePlacesPackage(),
             new MapsPackage()
       );
